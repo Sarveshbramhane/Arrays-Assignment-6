8e5aa843c437e87f6ee6c1419ee1294f3d598fee
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-23 19:11:18 +0530

    Update BugFix.java

--- BugFix.java
+++ BugFix.java
@@ -1,7 +1,9 @@
 public class BugFix{
   public static void main(String[]args){
-  int{}arr={2,3,4,5,6};
+  int []arr={2,3,4,5,6};
     for(int i=0;i<=arr.length;i++){
+      System.out.println(arr[i]);
       // Fill out the remaining code
   }
+}
 }
