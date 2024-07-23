4911937feb459b0b43bee7342ddea138b1937876
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-23 14:16:24 +0530

    Create ReturnArray.java

---
+++ ReturnArray.java
@@ -0,0 +1,11 @@
+public class ReturnArray{
+  
+public int[] printarr(int[]a){
+  // initialize []a here
+  return a;   
+    }
+  public static void main(String[]args){
+  ReturnArray r=new ReturnArray();
+    // call the function and print it here
+  }
+}
