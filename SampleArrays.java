f8dfde8dbe9ab7b1e29deb11d036977908e20f4c
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-23 19:17:25 +0530

    Update SampleArrays.java

--- SampleArrays.java
+++ SampleArrays.java
@@ -1,4 +1,20 @@
+import java.util.Arrays;
+
 public class SampleArrays{
+  public static void main(String[] args) {
+
+    int a[]={1,2,3,4,5,6};
+    System.out.println(a.length);
+    System.out.println(a[0]);
+    for(int i=0;i<a.length;i++){
+      System.out.println("enter the value for index:"+i);
+      System.out.println(a[i]);
+    }
+     System.out.println(Arrays.toString(a));
+}
+    
+  }
 // Declare and Initialize an Array
   // print an array using for loop
-}
+  
+
